dc46214152adf8f3942e49457a71e300397d433a
hiroshitake <yuutayuuta57@gmail.com>
2026-09-04 11:23:01 -0300

    feat: add security management route

---
+++ app/(admin)/manage-seguranca.tsx
@@ -0,0 +1,5 @@
+import ManageReportsFiltered from '../../components/admin/manage-reports-filtered';
+
+export default function ManageSegurancaScreen() {
+  return <ManageReportsFiltered security />;
+}
